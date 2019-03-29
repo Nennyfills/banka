b86e531439dec3afd288ac8320bcdeb4c573f37a
nennyfills <nennydike@gmail.com>
2019-03-29 15:51:57 +0100

    Updated Files

--- js-folder/index.js
+++ js-folder/index.js
@@ -2,24 +2,23 @@ const logMeIn = e => {
   e.preventDefault();
   let username = document.getElementById("username").value;
   let password = document.getElementById("userpassword").value;
+  let content = document.getElementById("login-respond");
   console.log(username, password);
 
   if (username === "admin" && password === "admin") {
-    alert("Login successfully");
+    modal.style.display = "block"
     window.location.href = "admin/dashboard.html";
     return false;
   } else if (username === "staff" && password === "staff") {
-    alert("Login successfully");
     window.location.href = "staff/dashboard.html";
     return false;
   } else if (username === "user" && password === "user") {
-    alert("Login successfully");
     window.location.href = "user/dashboard.html";
     return false;
   } else{
-    alert("wrong Username and Password")
+    content.style.display= "block";
+
   }
 };
 
 document.getElementById("loginSubmit").addEventListener("click", logMeIn);
-
